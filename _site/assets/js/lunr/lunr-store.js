9258a1eca5722c8c7cf7b8a6b3a777504463f033
projectrex <d3skt0p@gmail.com>
2025-10-06 21:03:54 -0700

    Remove generated _site directory and contents
    Deleted the entire _site directory including HTML, CSS, JS, images, and configuration files. This is typically done to clean up generated site artifacts or prepare for a fresh build.

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Fantastical Musings",
-        "excerpt":" ","categories": ["media"],
-        "tags": [],
-        "url": "/releases/fantastical_musings/",
-        "teaser": null
-      }]
